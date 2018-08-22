6ddd64236caa005eee0cdc85aee78abc0f3cac2f
David Thonnard <thonnard.david@gmail.com>
2018-08-22 20:45:13 +0200

    Update formatData.R

--- Functions/formatData.R
+++ Functions/formatData.R
@@ -3,11 +3,33 @@ formatData <- function(data){
   data <- read.csv(data, header = TRUE, sep = ",")
   
   # change column names
-  
+  names(data)[names(data)=="CorrectionTrials..1."] <- ""
+  names(data)[names(data)=="Schedule.run.date"] <- ""
+  names(data)[names(data)=="Duration..1."] <- ""
+  names(data)[names(data)=="Trials..1."] <- ""
+  names(data)[names(data)=="CorrectionTrials..1."] <- ""
+  names(data)[names(data)=="PercCorrect..1."] <- ""
+  names(data)[names(data)=="LeftITITouches..1."] <- ""
+  names(data)[names(data)=="RightITITouches..1."] <- ""
+  names(data)[names(data)=="Duration_10block..1."] <- ""
+  names(data)[names(data)=="Duration_10block..2."] <- ""
+  names(data)[names(data)=="Duration_10block..3."] <- ""
+  names(data)[names(data)=="CorrectTrials_10block..1."] <- ""
+  names(data)[names(data)=="CorrectTrials_10block..2."] <- ""
+  names(data)[names(data)=="CorrectTrials_10block..3."] <- ""
+  names(data)[names(data)=="PercCorrect_10block..1."] <- ""
+  names(data)[names(data)=="PercCorrect_10block..2."] <- ""
+  names(data)[names(data)=="PercCorrect_10block..3."] <- ""
+  names(data)[names(data)=="CorrectionTrials_10block..1."] <- ""
+  names(data)[names(data)=="CorrectionTrials_10block..2."] <- ""
+  names(data)[names(data)=="CorrectionTrials_10block..3."] <- ""
+
   # correct duration (comma vs dots)
   # if(x > 3700){x <- x/1000}
   
   # add extra columns (perseveration etc, SESSION!!)
+  # data$Session <- 
+  # data$Perseversation <-
   
   # order data
   data <- data[with(data, order(Animal,Session)), ]
@@ -15,3 +37,4 @@ formatData <- function(data){
   # save data
   write.csv(data, "data.cvs", row.names=FALSE)
 }
+
