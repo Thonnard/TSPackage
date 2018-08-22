3e49a86aa00fb2e9830d4efc1c3a06fb7f30da7b
David Thonnard <thonnard.david@gmail.com>
2018-08-22 20:04:59 +0200

    Create formatData.R

---
+++ Functions/formatData.R
@@ -0,0 +1,12 @@
+formatData <- function(data){
+  # read data
+  data <- read.csv(data, header = TRUE, sep = ",")
+  
+  # change column names
+  
+  # correct duration (comma vs dots)
+  # x <- x/1000
+  
+  # add extra columns (perseveration etc)
+  
+}
