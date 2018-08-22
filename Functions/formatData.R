078a2c7732a4cf2e4685783da8c230fd1548992c
David Thonnard <thonnard.david@gmail.com>
2018-08-22 21:03:26 +0200

    Update formatData.R

--- Functions/formatData.R
+++ Functions/formatData.R
@@ -2,30 +2,13 @@ formatData <- function(data){
   # read data
   data <- read.csv(data, header = TRUE, sep = ",")
   
-  # change column names
-  names(data)[names(data)=="CorrectionTrials..1."] <- "CorrectionTrials"
-  names(data)[names(data)=="Schedule.run.date"] <- ""
-  names(data)[names(data)=="Duration..1."] <- ""
-  names(data)[names(data)=="Trials..1."] <- ""
-  names(data)[names(data)=="CorrectionTrials..1."] <- ""
-  names(data)[names(data)=="PercCorrect..1."] <- ""
-  names(data)[names(data)=="LeftITITouches..1."] <- ""
-  names(data)[names(data)=="RightITITouches..1."] <- ""
-  names(data)[names(data)=="Duration_10block..1."] <- ""
-  names(data)[names(data)=="Duration_10block..2."] <- ""
-  names(data)[names(data)=="Duration_10block..3."] <- ""
-  names(data)[names(data)=="CorrectTrials_10block..1."] <- ""
-  names(data)[names(data)=="CorrectTrials_10block..2."] <- ""
-  names(data)[names(data)=="CorrectTrials_10block..3."] <- ""
-  names(data)[names(data)=="PercCorrect_10block..1."] <- ""
-  names(data)[names(data)=="PercCorrect_10block..2."] <- ""
-  names(data)[names(data)=="PercCorrect_10block..3."] <- ""
-  names(data)[names(data)=="CorrectionTrials_10block..1."] <- ""
-  names(data)[names(data)=="CorrectionTrials_10block..2."] <- ""
-  names(data)[names(data)=="CorrectionTrials_10block..3."] <- ""
-
-  # maybe it's better to work with a list and for loop, in case the column names differ depending on platform... (should work fine as long as order doesn't change)
-  coln <- c("Schedule","Box", "...")  # should contain every column name
+  # change column names (coln should contain every column name)
+  coln <- c("Protocol","Box", "RunTime", "Animal", "Group", "User", "Duration", "TotalTrials", "CorrectionTrials",
+            "PercCorrect", "LeftITITouches", "RightITITouches", "Latency_CorrectTouch", "Latency_IncorrectTouch", 
+            "Latency_CorrectLeftTouch", "Latency_CorrectRightTouch", "Latency_CorrectRewardCollection", 
+            "Duration_10block_1", "Duration_10block_2", "Duration_10block_3", "CorrectTrials_10block_1",
+            "CorrectTrials_10block_2", "CorrectTrials_10block_3", "PercCorrect_10block_1", "PercCorrect_10block_2", 
+            "PercCorrect_10block_3", "CorrectionTrials_10block_1", "CorrectionTrials_10block_2", "CorrectionTrials_10block_3")
   for (i in 1:ncol(data)) {names(data)[i] <- coln[i]}
   
   # correct duration (comma vs dots)
